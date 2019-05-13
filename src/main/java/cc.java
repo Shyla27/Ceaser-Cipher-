5723f7e4418bd7b8d0a291d84fd52d4ef4169f15
SHYLA <sheilabernicew@gmail.com>
2019-05-13 08:18:21 +0300

    initial commit

---
+++ src/main/java/cc.java
@@ -0,0 +1,51 @@
+package cc;
+
+import java.util.Scanner;
+
+public class CeaserCipher {
+    // This rotates a character k-positions
+
+    private String mText;
+    private int mShift;
+
+    public CeaserCipher(String mText, int mShift) {
+        this.mText = mText;
+        this.mShift = mShift;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public int getShift() {
+        return mShift;
+    }
+
+    public static char cipher(char c, int k) {
+        final int alphaLength = 26;
+        final char asciiShift = Character.isUpperCase(c) ? 'A' : 'a';
+        final int cipherShift = k % alphaLength;
+
+        char shifted = (char) (c - asciiShift);
+
+        shifted = (char) ((shifted + cipherShift + alphaLength) % alphaLength);
+
+        return (char) (shifted + asciiShift);
+    }
+
+    public  String cipher() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < mText.length(); i++) {
+            if(mText.charAt(i) == ' ')
+            {
+                sb.append(" ");
+            }
+            else
+            {
+                sb.append(cipher(mText.charAt(i), mShift));
+            }
+
+        }
+        return sb.toString();
+    }
+}
